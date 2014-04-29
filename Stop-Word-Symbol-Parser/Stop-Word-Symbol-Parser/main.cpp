dbee15d6254fadb76e7749eead94b9207922c651
AayushB <Aayush@purpamine.bh>
2014-04-28 18:18:22 -0700

    Setup xcode c++ files

---
+++ Stop-Word-Symbol-Parser/Stop-Word-Symbol-Parser/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Stop-Word-Symbol-Parser
+//
+//  Created by Aayush Bhandari on 4/28/14.
+//  Copyright (c) 2014 Aayush Bhandari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
